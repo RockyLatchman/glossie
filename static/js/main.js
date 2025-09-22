60a882f17401e6473e0c0a85ebbd199b6d824143
RockyLatchman <r.latchman3624@gmail.com>
2025-09-22 17:26:44 +0700

    Navigation menu CSS and JavaScript code

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 const letters = document.querySelectorAll('[data-initial]');
 const letter = document.querySelector('#letter h1');
 const glossaryIndex = document.querySelectorAll('.index menu li a');
+const hamburgerMenu = document.querySelector('nav ul li');
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach((entry, index) => {
@@ -19,4 +20,30 @@ const observer = new IntersectionObserver(entries => {
 
 letters.forEach(letter => {
   observer.observe(letter);
+})
+
+function loadMenu() {
+  const navMenu = `
+     <div class="menu-window">
+       <a href="" id="close">[X] Close</a>
+       <menu>
+         <li><a href="/about">About</a></li>
+         <li><a href="/documentation">Documentation</a></li>
+         <li><a href="/newsletter">Newsletter</a></li>
+       </menu>
+     </div>
+  `;
+  // refactor this
+  const template = document.createElement('template');
+  template.innerHTML = navMenu;
+  document.body.appendChild(template.content);
+  document.querySelector('.menu-window').style.opacity = 0;
+  // end
+  document.querySelector('#close').addEventListener('click', (e) => {
+       document.querySelector('.menu-window').setAttribute('id', 'menu-window');
+  });
+}
+
+hamburgerMenu.addEventListener('click', (e) => {
+  loadMenu();
 })
